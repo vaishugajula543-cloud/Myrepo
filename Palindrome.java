ec568e2363ea83410f031b252df5b96e370407be
vaishugajula543-cloud <vaishugajula543@gmail.com>
2025-09-19 15:51:28 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        if (input.equals(reversed)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
